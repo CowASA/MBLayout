a1ae8650e44a541fe9960e5274c1851d6e3d7e2c
Jesse Walter <CowTokenASA@gmail.com>
2022-02-02 11:43:39 -0600

    Theme

--- MBExample/MBExample.App/Program.cs
+++ MBExample/MBExample.App/Program.cs
@@ -2,6 +2,7 @@ using MBExample.App;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
