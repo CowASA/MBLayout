b0558db90a4a6e2c95c08b8a0711853ed742bdea
Jesse Walter <CowTokenASA@gmail.com>
2022-02-06 13:12:03 -0600

    Added options to attempt to fix error

--- MBExample/MBExample.App.Services/AccountInfoService.cs
+++ MBExample/MBExample.App.Services/AccountInfoService.cs
@@ -2,16 +2,13 @@
 using MBExample.App.Services.Interfaces;
 using MBExample.App.Shared.Models;
 using MBExample.App.Shared.Responses;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.ComponentModel;
 using System.Net.Http.Json;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace MBExample.App.Services
 {
-    public class AccountInfoService : IAccountInfo
+	public class AccountInfoService : IAccountInfo
     {
         private readonly HttpClient _httpClient;
 
@@ -29,9 +26,16 @@ namespace MBExample.App.Services
         {
             var response = await _httpClient.GetAsync($"https://algoindexer.algoexplorerapi.io/v2/accounts/{id}");
 
+            var options = new JsonSerializerOptions()
+            {
+                NumberHandling = System.Text.Json.Serialization.JsonNumberHandling.AllowReadingFromString |
+                System.Text.Json.Serialization.JsonNumberHandling.WriteAsString
+            };
+
             if (response.IsSuccessStatusCode)
             {
-                var result = await response.Content.ReadFromJsonAsync<AccountInfo>();
+                var result = await response.Content.ReadFromJsonAsync<AccountInfo>(options);
+
                 return result;
             }
             else
